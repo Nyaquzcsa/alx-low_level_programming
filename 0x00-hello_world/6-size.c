2262fa900aa694d1b5fbcee009588d93426477d7
Nyaquzcsa <nyaquwaniqwa@gmail.com>
2023-05-18 14:58:31 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,20 +4,21 @@
  * main – a program that calculate the sizes of  Data Types for current system
  * Return 0 (Success)
 */
-int main()
+int main (void)
 {
-	int a;
-	float b;
-	char c;
-	long int d;
-	long long int e;
 
-		printf("Size of int: %lu byte(s)\n", sizeof(a));
-		printf("Size of float: %lu byte(s)\n", sizeof(b));
-		printf("Size of char: %lu byte(s)\n", sizeof(c));
-		printf("Size of long int: %lu byte(s)\n", sizeof(d));
-		printf("Size of long long int: %lu byte(s)\n", sizeof(e));
+    int intType;
+    float floatType;
+    char charType;
+    long int longType;
+    long long int longlongType;
 
-		return (0);
+    printf("Size of int: %lu byte(s)\n", sizeof(intType));
+    printf("Size of float: %lu byte(s)\n", sizeof(floatType));
+    printf("Size of long int: %lu byte(s)\n"sizeof(longType));
+    printf("Size of char: %lu byte(s)\n", sizeof(charType));
+    printf("Size of long long int: %lu byte(s)\n"sizeof(longlongType));
+
+    return 0;
 
 }
