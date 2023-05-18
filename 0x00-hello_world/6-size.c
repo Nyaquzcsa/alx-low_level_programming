106e0ca5c7d179c5e2bf98e4aea467d5b2114795
Nyaquzcsa <nyaquwaniqwa@gmail.com>
2023-05-18 16:12:36 -0700

    1 and 5 fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,15 @@
 #include <stdio.h>
-
-
-int main (void)
-{
 /**
- * main – a program that calculate the sizes of  Data Types for current system
+ * main – a program that calculate the sizes of
+ * Data Types for current system
  * Return 0 (Success)
-*/
-
-	int a;
-	float b;
-	char c;
-	long int d;
-	long long int e;
-
-		printf("Size of int: %lu byte(s)\n", sizeof(a));
-		printf("Size of float: %lu byte(s)\n", sizeof(b));
-		printf("Size of char: %lu byte(s)\n", sizeof(c));
-		printf("Size of long int: %lu byte(s)\n", sizeof(d));
-		printf("Size of long long int: %lu byte(s)\n", sizeof(e));
-    return 0;
-
+ */
+int main(void)
+{
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %lu bytes\n", sizeof(long long int));
+	return (0);
 }
