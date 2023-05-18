6199909065ebe8d4aae44626fac9c1928d6c50e5
Nyaquzcsa <nyaquwaniqwa@gmail.com>
2023-05-18 10:13:02 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+/*pointless procedure for commenting on elements the checker doesnt need*/
+int main(void)
+{
+puts("\ "Programming is like building a multilingual puzzle");
+return(0);
+}
