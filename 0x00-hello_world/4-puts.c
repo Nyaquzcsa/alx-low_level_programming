840e986a86352b6aac3ff3377d44baeeb5bf500c
Nyaquzcsa <nyaquwaniqwa@gmail.com>
2023-05-18 15:47:31 -0700

    1 and 5 fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,9 @@
 /**
  * main- function is to use puts to print string of characters.
  * Return 0 (Success).
-*/
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-		/**
-		 *  puts function functions in a similar way to printf
-		 */
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
