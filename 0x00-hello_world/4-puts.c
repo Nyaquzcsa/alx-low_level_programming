2297490a287c1e8e5b2034c7881f1f77ba762f95
Nyaquzcsa <nyaquwaniqwa@gmail.com>
2023-05-18 10:36:37 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
-/*pointless procedure for commenting on elements the checker doesnt need*/
+#include <stdio.h>
+/* main is the starting point of any c programcompilation*/
 int main(void)
 {
-puts("\ "Programming is like building a multilingual puzzle");
-return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
