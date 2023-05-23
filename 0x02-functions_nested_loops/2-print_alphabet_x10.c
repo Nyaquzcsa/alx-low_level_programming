594155901622a124f0484fac9deccd8cbec3a545
Nyaquzcsa <nyaquwaniqwa@gmail.com>
2023-05-23 10:50:33 -0700

    not halfway yet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * printAlphabet - prints the alphabet in lowercase.
+ */
+void printAlphabet(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	}
+}
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printAlphabet();
+	return (0);
+}
