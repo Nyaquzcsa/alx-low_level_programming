339f56f08ccc9ec6f467e3c09475a45d9db9c6f0
Nyaquzcsa <nyaquwaniqwa@gmail.com>
2023-05-24 01:03:59 -0700

    10th times the charm

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,14 @@
 #include "main.h"
+
 /**
- * main - prints _putchar
- * Return: 0 Success.
+ * main - print _putchar
+ * Return: 0 Success
  */
 
 int main(void)
+
 {
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -15,5 +18,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-return (0);
+	return (0);
+
 }
